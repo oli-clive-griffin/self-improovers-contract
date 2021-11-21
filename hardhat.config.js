8e8b54832d146f0617ffd21fb68cc23da5482555
Oli <o.clivegriffin@gmail.com>
2021-11-21 19:19:11 +1300

    no real money anyway lol

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -8,7 +9,7 @@ module.exports = {
   networks: {
     rinkeby: {
       url: 'https://eth-rinkeby.alchemyapi.io/v2/CQQZhFMGCjf7p-6aC_kEqC27kp9l3emU',
-      accounts: ['3a23a15d38d5276b6b791566741cbc989576901d0249ee5b479b0a62ab718cde'],
+      accounts: [process.env.SHIT_ACCOUNT_PRIVATE_KEY],
     },
   }
 };
